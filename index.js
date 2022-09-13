c268b887f712a9cd13949cbfdbbd6c1418666276
Benny Sufrin <bennysuf@gmail.com>
2022-09-13 18:22:30 -0400

    commit tree f7fe6aaf01a0f6c399ab460342fafd89f107ce0a

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  //iterate through each number one at a time starting with the first
+//  const equals = (element) => {
+//   console.log(element)
+  const equals = () => {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j]=== target) {
+       return true
+      }
+    //  return array[i] + array[j] === target
+  //   // look through the rest of the numbers
+  //   let total = array[i] + array[j] === target
+  //   // console.log(total)
+    
+    
+  }
+ 
+}
+  }
+ return array.some(equals)
+
 }
 
+// hasTargetSum([1, 2, 3], 6)
+
+/*
+ ```
+PROBLEMS:
+how to get single output 
+how to iterate till true and only give single output at the end
+ */
+
+
+
+
 /* 
+The function should return true if any pair of numbers in the array adds up to the target number
+
   Write the Big O time complexity of your function here
 */
 
 /* 
   Add your pseudocode here
+  if i + j === targer .some() should return true
+  // or do a .forEach() 
+  iterate through the numbers giving the first the value of i and the value of j should iterate 
+  how to return false after iterator?  
 */
 
 /*
